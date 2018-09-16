11f89958431544a3646058dbf88143a09d4cf082
Yizhen Shi <yizhen@Yizhens-MacBook-Air.local>
2018-09-15 23:20:36 -0400

    four different stock and basic UI

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Menu,ipcMain} = require('electron')
 const path = require('path')
 const url = require('url')
 
-let window = null
+
+let mainWindow = null;
+let ChangeSyWindow = null;
+
 
 // Wait until the app is ready
 app.once('ready', () => {
   // Create a new window
-  window = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     // Set the initial width to 800px
     width: 400,
     // Set the initial height to 600px
-    height: 500,
+    height: 455,
     // Set the default background color of the window to match the CSS
     // background color of the page, this prevents any white flickering
     
@@ -23,14 +26,92 @@ app.once('ready', () => {
   })
 
   // Load a URL in the window to the local index.html path
-  window.loadURL(url.format({
+  mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
   }))
 
   // Show window when page is ready
-  window.once('ready-to-show', () => {
-    window.show()
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show()
   })
+
+  //Build menu from template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  // Insert menu
+  Menu.setApplicationMenu(mainMenu);
 })
+
+// create menu template
+const mainMenuTemplate = [
+  {
+   
+    label:'File',
+    submenu:[
+      {
+        label: 'Change Company symbol',
+        click(){
+          createChangeSyWindow();
+        }
+      },
+      {
+        label: 'Set Your Buy in Price'
+      },
+      {
+        label: 'Quit',
+        accelerator: process.platform == 'darwin' ? 'Command + Q': 'Ctrl+Q', 
+        click(){
+          app.quit();
+        }
+      }
+    ]
+  }
+];
+// If mac, add empty object to menu
+if(process.platform == 'darwin'){
+  mainMenuTemplate.unshift({});
+}
+//Add dev tools item if not in prod mode
+if(process.env.NODE_ENV !== 'production'){
+  mainMenuTemplate.push({
+    label: 'Develop Tools',
+    submenu:[
+      {
+        label: 'Toggle DevTools',
+        accelerator: process.platform == 'darwin' ? 'Command + T': 'Ctrl+T', 
+        click(item, focusedWindow){
+          focusedWindow.toggleDevTools();
+        }
+      },
+      {
+        role: 'reload'
+      }
+    ]
+  });
+}
+
+//  change company symbol windows
+function createChangeSyWindow(){
+  // Create a new window
+  ChangeSyWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title:'Change company symbol',
+    //resizable: false,
+  });
+  ChangeSyWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'ChangeSy.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+  //Garbage collection handle
+  ChangeSyWindow.on('close',function(){
+    ChangeSyWindow = null;
+  })
+}
+
+
+
+
+
--- monitor.js
+++ monitor.js
@@ -1,3 +1,8 @@
+const urlP1 = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=";
+const urlP2 = "&interval=1min&apikey=0CZBXPGPXL2CBG2Q";
+
+
+
 var request = require ('request');
 request("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AAPL&interval=1min&apikey=0CZBXPGPXL2CBG2Q", function(error,response,body){
     
@@ -5,22 +10,69 @@ request("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=
     companySymbol(body);
     newPrice(body);
 });
-function companySymbol(arr){
+
+var R1 = require ('request');
+var R2 = require ('request');
+var R3 = require ('request');
+var R4 = require ('request');
+
+R1(urlP1+"AAPL"+urlP2, function(error,response,body){
+    
+    body = JSON.parse(body);
+    companySymbol(body,1);
+    newPrice(body,1);
+
+});
+
+R2(urlP1+"GOOG"+urlP2, function(error,response,body){
+    
+   body = JSON.parse(body);
+
+    companySymbol(body,2);
+    newPrice(body,2);
+
+});
+
+R3(urlP1+"SNAP"+urlP2, function(error,response,body){
+    
+    body = JSON.parse(body);
+ 
+     companySymbol(body,3);
+     newPrice(body,3);
+ 
+ });
+ R4(urlP1+"F"+urlP2, function(error,response,body){
+    
+    body = JSON.parse(body);
+ 
+     companySymbol(body,4);
+     newPrice(body,4);
+ 
+ });
+ 
+
+
+
+
+function companySymbol(arr,int){
     var A = arr["Meta Data"]
     var B = Object.keys(A).map(i => A[i])
-    
+    var row = int;
      var companySymbol = B[1]
-     document.getElementById("symbol").innerHTML = companySymbol
+     document.getElementById("symbol"+row).innerHTML = "Symbol: "+companySymbol 
+     
 
      
 }
-function newPrice(arr){
+function newPrice(arr,int){
     // FIRST LAYER OBJECT
     var A = arr["Time Series (1min)"]
     // SECOND LAYER OBJECT
     var B = Object.keys(A).map(i => A[i])
+    var row = int;
     var newPrice = parseFloat(B[0]["4. close"])
-    document.getElementById("price").innerHTML = newPrice
+    document.getElementById("price"+row).innerHTML = newPrice
+
 
 }
 
